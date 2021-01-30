d7cfe4ab6c89b5098c70a12a5073f8d0813adf11
Shailesh Aanand <anaandshailu@gmail.com>
2021-01-30 17:33:56 +0000

    render todos page correctly

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,4 +2,20 @@ class Todo < ActiveRecord::Base
   def to_pleasant_string
     "#{id}. #{due_date.to_s(:short)} #{todo_text} #{completed ? "[X]" : "[ ]"}"
   end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.due_today
+    all.where(due_date: Date.today)
+  end
+
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
 end
