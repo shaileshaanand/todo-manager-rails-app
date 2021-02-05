d263e31182f503089c647d4bae1f696f84d584e1
Shailesh Aanand <anaandshailu@gmail.com>
2021-02-05 09:15:41 +0000

    Fixed some forms

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,5 @@
 # todos_controller.rb
 class TodosController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
   def index
     # render plain: Todo.all.map { |todo| todo.to_pleasant_string }.join("\n")
     render "index"
