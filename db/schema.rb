03a69dc215431fd2a5d5fe6799d3dc3086b42334
Shailesh Aanand <anaandshailu@gmail.com>
2021-02-05 09:21:39 +0000

    create user table migration

--- db/schema.rb
+++ db/schema.rb
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 2021_01_19_134849) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
